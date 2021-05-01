1322754a49207916e7bbec16e376727e6789c47f
Suvankar <suvankarpradhan100@gmail.com>
2021-05-01 18:36:24 +0530

    update mobile version navbar

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function nav() {
     var x = document.getElementById("nav");
     if (x.className === "navbar") {
       x.className += " responsive";
+      $('.icon>img').attr('src','images/icon-close.svg');
+      $('.dropbtn>img').attr('src','images/icon-arrow-dark.svg');
     } else {
       x.className = "navbar";
+      $('.icon>img').attr('src','images/icon-hamburger.svg');
+      $('.dropbtn>img').attr('src','images/icon-arrow-light.svg');
     }
   }
